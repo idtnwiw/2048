bdf1bd77a2bdf752de19b755c4a47fb28846d3c0
idtnwiw <idtnwiw@gmail.com>
2017-11-01 19:00:53 +0800

    second assignment

--- app/src/main/java/com/example/nxwhy/a2048/Card.java
+++ app/src/main/java/com/example/nxwhy/a2048/Card.java
@@ -2,8 +2,12 @@ package com.example.nxwhy.a2048;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import static android.widget.ListPopupWindow.WRAP_CONTENT;
 
 /**
  * Created by nxwhy on 2017/10/19.
@@ -11,8 +15,87 @@ import android.widget.TextView;
 
 public class Card extends FrameLayout {
 
-    public Card(@NonNull Context context) {
+    TextView tv;
+    View view;
+
+    public Card(@NonNull Context context,int w) {
         super(context);
-        TextView tv = new TextView(context);
+        FrameLayout.LayoutParams lp =new FrameLayout.LayoutParams(w-10,w-10);
+        lp.setMargins(4,4,4,4);
+
+        view = new View(context);
+        //view.setBackgroundColor(0x33ff00ff);
+        addView(view,lp);
+
+        tv = new TextView(context);
+        //tv.setText(Integer.toString(0));
+        tv.setTextSize(48);
+        tv.setGravity(Gravity.CENTER);
+        addView(tv);
+    }
+
+    int value;
+
+    public void setNum(int num)
+    {
+        value = num;
+        if(num>0)
+        {
+            tv.setText(Integer.toString(num));
+            switch(num)
+            {
+                case 2:
+                    view.setBackgroundColor(0xffeee4da);
+                    tv.setTextColor(0xff000000);
+                    break;
+                case 4:
+                    view.setBackgroundColor(0xffede0c8);
+                    tv.setTextColor(0xff000000);
+                    break;
+                case 8:
+                    view.setBackgroundColor(0xfff2b179);
+                    tv.setTextColor(0xffffffff);
+                    break;
+                case 16:
+                    view.setBackgroundColor(0xfff59563);
+                    tv.setTextColor(0xffffffff);
+                    break;
+                case 32:
+                    view.setBackgroundColor(0xfff67c5f);
+                    tv.setTextColor(0xffffffff);
+                    break;
+                case 64:
+                    view.setBackgroundColor(0xfff65e3b);
+                    tv.setTextColor(0xffffffff);
+                    break;
+                case 128:
+                    view.setBackgroundColor(0xffedcf72);
+                    tv.setTextColor(0xffffffff);
+                    break;
+                case 256:
+                    view.setBackgroundColor(0xffedcc61);
+                    tv.setTextColor(0xffffffff);
+                    break;
+                case 512:
+                    view.setBackgroundColor(0xffedc850);
+                    tv.setTextColor(0xffffffff);
+                    break;
+                case 1024:
+                    view.setBackgroundColor(0xffedc53f);
+                    tv.setTextColor(0xffffffff);
+                    break;
+            }
+        }
+
+        else
+        {
+            tv.setText("");
+            view.setBackgroundColor(0xffcdc1b4);
+        }
+    }
+
+    public int getNum()
+    {
+        return value;
     }
 }
--- app/src/main/java/com/example/nxwhy/a2048/MainActivity.java
+++ app/src/main/java/com/example/nxwhy/a2048/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.nxwhy.a2048;
 
+import android.graphics.Point;
+import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import static android.widget.ListPopupWindow.WRAP_CONTENT;
 
@@ -17,34 +25,248 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler h = new Handler();
+        h.postDelayed(r,500);
+    }
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+            myfun();
+            }
+        };
+
+        float sx,sy;
+        void myfun()
+    {
+        ConstraintLayout myview = (ConstraintLayout) findViewById(R.id.myview);
 
         LinearLayout li =new LinearLayout(getApplicationContext());
         ViewGroup.LayoutParams params =new LinearLayout.LayoutParams(WRAP_CONTENT,WRAP_CONTENT);
         li.setOrientation(LinearLayout.VERTICAL);
-        addContentView(li,params);
-        for(int i=0;i<4;i++)
+        //addContentView(li,params);
+        myview.addView(li,params);
+
+        myview.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                float ex,ey;
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        sx=event.getX();
+                        sy=event.getY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        ex=event.getX()-sx;
+                        ey=event.getY()-sy;
+                        if(Math.abs(ex)>Math.abs(ey))
+                        {
+                            if(ex>0)
+                                mright();
+                            else
+                                mleft();
+
+                        }
+                        else
+                        {
+                            if(ey<0)
+                                mup();
+                            else
+                                mdown();
+
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
+
+        int w = myview.getWidth();
+        int n=4;
+        int nwidth = w/n;
+        for(int j=0;j<n;j++)
         {
             LinearLayout row =new LinearLayout(getApplicationContext());
-            
+
             row.setOrientation(LinearLayout.HORIZONTAL);
 
-            for(int j=0;j<4;j++) {
-                FrameLayout fl = new FrameLayout(getApplicationContext());
-                FrameLayout.LayoutParams lp =new FrameLayout.LayoutParams(WRAP_CONTENT,WRAP_CONTENT);
-                lp.setMargins(10,10,10,10);
-
-                TextView tv = new TextView(getApplicationContext());
-                tv.setText(Integer.toString(i*4+j));
-                tv.setTextSize(28);
-                tv.setGravity(Gravity.CENTER);
-                fl.addView(tv);
-                row.addView(fl,lp);
+            for(int i=0;i<n;i++) {
+                Card fl = new Card(getApplicationContext(),nwidth);
+                Cards[j][i]=fl;
+                fl.setNum(0);
+                //fl.setNum(j*n+i);
+                row.addView(fl);
             }
             li.addView(row);
         }
 
-        Card obj = new Card(getApplicationContext());
+        //Card obj = new Card(getApplicationContext());
+        initGame();
+        addNum();
+        addNum();
 
+        addNum();
+        addNum();
 
     }
+
+    void mright(){
+        boolean merge = false;
+        for(int j=0;j<nrows;j++) {
+            for (int i = nrows - 1; i >= 0; i--) {
+                OUT:
+                for (int ni = i - 1; ni >= 0; ni--) {
+                    int curri = Cards[j][i].getNum();
+                    int checki = Cards[j][ni].getNum();
+                    if (checki > 0) {
+                        if (curri == 0) {
+                            Cards[j][i].setNum(checki);
+                            Cards[j][ni].setNum(0);
+                            merge = true;
+                            i++;
+                            break;
+                        } else if (checki == curri) {
+                            Cards[j][i].setNum(checki * 2);
+                            Cards[j][ni].setNum(0);
+                            merge = true;
+                        }
+                    }
+                }
+            }
+        }
+        //if(merge)
+            addNum();
+        Toast.makeText(this, "right", Toast.LENGTH_SHORT).show();
+    }
+    void mleft(){
+        boolean merge = false;
+        for(int j=0;j<nrows;j++) {
+            for (int i = 0; i < nrows; i++) {
+                OUT:
+                for (int ni = i + 1; ni < nrows; ni++) {
+                    int curri = Cards[j][i].getNum();
+                    int checki = Cards[j][ni].getNum();
+                    if (checki > 0) {
+                        if (curri == 0) {
+                            Cards[j][i].setNum(checki);
+                            Cards[j][ni].setNum(0);
+                            merge = true;
+                            i--;
+                            break;
+                        } else if (checki == curri) {
+                            Cards[j][i].setNum(checki * 2);
+                            Cards[j][ni].setNum(0);
+                            merge = true;
+                        }
+                    }
+                }
+            }
+        }
+        //if(merge)
+            addNum();
+        Toast.makeText(this, "left", Toast.LENGTH_SHORT).show();
+
+    }
+    void mup(){
+
+        boolean merge = false;
+        for(int i=nrows-1;i>=0;i--) {
+            for (int j = 0; j < nrows; j++) {
+                OUT:
+                for (int nj = j + 1; nj < nrows; nj++) {
+                    int curri = Cards[j][i].getNum();
+                    int checki = Cards[nj][i].getNum();
+                    if (checki > 0) {
+                        if (curri == 0) {
+                            Cards[j][i].setNum(checki);
+                            Cards[nj][i].setNum(0);
+                            merge = true;
+                            j--;
+                            break;
+                        } else if (checki == curri) {
+                            Cards[j][i].setNum(checki * 2);
+                            Cards[nj][i].setNum(0);
+                            merge = true;
+                        }
+                    }
+                }
+            }
+        }
+        //if(merge)
+            addNum();
+        Toast.makeText(this, "up", Toast.LENGTH_SHORT).show();
+
+    }
+    void mdown(){
+
+        boolean merge = false;
+        for(int i=nrows-1;i>=0;i--) {
+            for (int j = nrows - 1; j >= 0; j--) {
+                OUT:
+                for (int nj = j - 1; nj >= 0; nj--) {
+                    int curri = Cards[j][i].getNum();
+                    int checki = Cards[nj][i].getNum();
+                    if (checki > 0) {
+                        if (curri == 0) {
+                            Cards[j][i].setNum(checki);
+                            Cards[nj][i].setNum(0);
+                            merge = true;
+                            j++;
+                            break;
+                        } else if (checki == curri) {
+                            Cards[j][i].setNum(checki * 2);
+                            Cards[nj][i].setNum(0);
+                            merge = true;
+                        }
+                    }
+                }
+            }
+        }
+        //if(merge)
+            addNum();
+        Toast.makeText(this, "down", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+    int nrows=4;
+    Card[][] Cards =new Card[nrows][nrows];
+
+    ArrayList<Point> emptyList = new ArrayList<Point>();
+
+    void initGame()
+    {
+        for(int j=0;j<nrows;j++)
+            for (int i=0;i<nrows;i++)
+            {
+                emptyList.add(new Point(i,j));
+            }
+
+    }
+
+    void addNum()
+    {
+        if(emptyList.size()>0)
+        {
+            boolean right=true;
+            while(right){
+                int r=(int)(Math.random()*emptyList.size());
+                Point p =emptyList.remove(r);
+                if(Cards[p.x][p.y].getNum()>0)
+                {
+                    right=true;
+                }
+                else
+                {
+                    Cards[p.x][p.y].setNum(2);
+                    right=false;
+                }
+            }
+
+        }
+
+    }
+
 }
